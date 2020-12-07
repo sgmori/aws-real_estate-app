876d3439d9167e17e257398a95b124e437799f97
Samuel Mori <samuel.mori@gmail.com>
2020-12-07 08:43:38 +0000

    updated

---
+++ flask-app/application.py
@@ -0,0 +1,35 @@
+from flask import Flask, request, render_template
+
+application = app = Flask(__name__)
+
+@application.route('/')
+def index():
+    return "This is the main page."
+
+@app.route('/Charts')
+def line():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    application.run(debug=True)
+
+
+
+
+
+# from flask import Flask, jsonify
+# import aws_controller
+# import os
+
+# application = app = Flask(__name__)
+
+# @app.route('/')
+# def index():
+#     return "This is the main page."
+    
+# @app.route('/get-items')
+# def get_items():
+#     return jsonify(aws_controller.get_items())
+
+# if __name__ == '__main__':
+#     app.run()

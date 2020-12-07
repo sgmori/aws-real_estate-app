f57487608025cbb2deeec7aac585f523ebe1e814
Samuel Mori <samuel.mori@gmail.com>
2020-12-07 10:49:28 +0000

    updated

--- flask-app/application.py
+++ flask-app/application.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, render_template
+import jsonify
 
 application = Flask(__name__)
 
@@ -9,6 +10,11 @@ def index():
 @application.route('/test')
 def line():
     return render_template('index.html')
+    
+@application.route('/echo/<name>')
+def echo(name):
+    val = {"new name": name}
+    return jsonify(val)
 
 if __name__ == '__main__':
     application.run(debug=True)

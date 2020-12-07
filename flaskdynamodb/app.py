fd68d692ed5814f1ecc0fe47972a8e61741bb9ce
Samuel Mori <samuel.mori@gmail.com>
2020-12-07 07:12:02 +0000

    updated requirements.txt

--- flaskdynamodb/app.py
+++ flaskdynamodb/app.py
@@ -13,4 +13,4 @@ def get_items():
     return jsonify(aws_controller.get_items())
 
 if __name__ == '__main__':
-    app.run(host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 8080)))
+    app.run()
--- flaskdynamodb/aws_controller.py
+++ flaskdynamodb/aws_controller.py
@@ -1,6 +1,6 @@
 import boto3
 
-dynamo_client = boto3.client('dynamodb')
+dynamo_client = boto3.client('dynamodb', region_name='us-west-2')
 
 def get_items():
     return dynamo_client.scan(

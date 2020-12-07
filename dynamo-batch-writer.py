3fbfe7bf1e37c08a6dd7848c4e4e4fbc65427b73
Samuel Mori <samuel.mori@gmail.com>
2020-12-07 02:37:39 +0000

    Adding flaskdynamodb folder

---
+++ dynamo-batch-writer.py
@@ -0,0 +1,35 @@
+import boto3
+import csv
+
+dynamodb = boto3.resource('dynamodb', 'us-west-2')
+
+
+def batch_writer(table_name, rows):
+    table = dynamodb.Table(table_name)
+    
+    with table.batch_writer() as batch:
+        for row in rows:
+            batch.put_item(Item=row)
+    return True
+    
+    
+def read_csv(csv_file, list):
+    rows = csv.DictReader(open(csv_file))
+    
+    for row in rows:
+        list.append(row)
+        
+        
+if __name__ == '__main__':
+    
+    table_name = 'maricopa-sales-data'
+    file_name = 'MaricopaPropertySales_Jan.csv'
+    items = []
+    
+    read_csv(file_name, items)
+    status = batch_writer(table_name, items)
+    
+    if(status):
+        print('Data is saved')
+    else:
+        print('Error while inserting data')

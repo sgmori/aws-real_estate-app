9524ff9c0bf63bdef57db441efce024a537f41af
Samuel Mori <samuel.mori@gmail.com>
2020-12-07 04:47:28 +0000

    updated

--- flaskdynamodb/app.py
+++ flaskdynamodb/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify
 import aws_controller
+import os
 
 app = Flask(__name__)
 
@@ -11,6 +12,5 @@ def index():
 def get_items():
     return jsonify(aws_controller.get_items())
 
-
 if __name__ == '__main__':
-    app.run()
+    app.run(host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 8080)))
